74fd479613149a4ef0c1a9f4fb4d341421d25616
leszekl <leszek.lugin@intel.com>
2018-09-23 19:19:36 +0200

    introduce blob interface

--- pmemobj/pmemobj.go
+++ pmemobj/pmemobj.go
@@ -1,35 +1,3 @@
-/*
- * Copyright 2018, Intel Corporation
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in
- *       the documentation and/or other materials provided with the
- *       distribution.
- *
- *     * Neither the name of the copyright holder nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package pmemobj
 
 // #cgo LDFLAGS: -lpmemobj
@@ -55,9 +23,20 @@ import (
 	"unsafe"
 )
 
+// Blob of data allocable on persistent pool
+type Blob interface {
+	// Pointer to underlying data
+	Ptr() unsafe.Pointer
+	// Set value of underlying data based on provided unsafe pointer
+	Set(p unsafe.Pointer)
+	// Size of underlying data
+	Size() uintptr
+}
+
 // Pool is a handler to pmemobj pool
 type Pool struct {
-	pop *C.struct_pmemobjpool
+	pop  *C.struct_pmemobjpool
+	open bool
 }
 
 // Oid is a handler to object allocated on pmemobj pool
@@ -67,35 +46,38 @@ type Oid struct {
 
 // Create creates new pmemobj pool
 func Create(path, layout string, size uint64, mode uint16) (Pool, error) {
-	pop, err := C.pmemobj_create(C.CString(path), C.CString(layout), C.size_t(size), C.uint(mode))
-	return Pool{pop}, err
+	pop, err := C.pmemobj_create(C.CString(path), C.CString(layout),
+		C.size_t(size), C.uint(mode))
+	return Pool{pop, pop != nil}, err
 }
 
 // Open opens existing pmemobj pool
 func Open(path, layout string) (Pool, error) {
 	pop, err := C.pmemobj_open(C.CString(path), C.CString(layout))
-	return Pool{pop}, err
+	return Pool{pop, pop != nil}, err
 }
 
 // Close closes the pmemobj pool
 func (p *Pool) Close() {
 	C.pmemobj_close(p.pop)
+	p.open = false
 }
 
-// Alloc allocates new value on pmemobj pool
-func (p *Pool) Alloc(value int) (Oid, error) {
+// AllocBlob allocates new value on pmemobj pool
+func (p *Pool) AllocBlob(value Blob) (Oid, error) {
 	var oid C.struct_pmemoid
 
-	data := C.struct_data{C.size_t(unsafe.Sizeof(value)), unsafe.Pointer(&value)}
+	data := C.struct_data{C.size_t(value.Size()), value.Ptr()}
 	cptr := C.malloc(C.size_t(unsafe.Sizeof(data)))
 	if cptr == nil {
-		return Oid{}, fmt.Errorf("C.malloc fail")
+		return Oid{}, fmt.Errorf("C.malloc failed")
 	}
 	defer C.free(cptr)
 
 	*(*C.struct_data)(cptr) = data
 
-	i, err := C.pmemobj_alloc(p.pop, &oid, C.size_t(data.size), 1, C.pmemobj_constr(C.elem_constructor), cptr)
+	i, err := C.pmemobj_alloc(p.pop, &oid, C.size_t(value.Size()),
+		1, C.pmemobj_constr(C.elem_constructor), cptr)
 	if i != 0 {
 		return Oid{}, err
 	}
@@ -103,12 +85,22 @@ func (p *Pool) Alloc(value int) (Oid, error) {
 	return Oid{oid}, nil
 }
 
-// First returns first object from the pool
-func (p *Pool) First() (int, error) {
+// FirstBlob returns first object from the pool
+func (p *Pool) FirstBlob(b Blob) (Oid, error) {
 	oid := C.pmemobj_first(p.pop)
 	if oid.off == 0 {
-		return 0, fmt.Errorf("oid is null")
+		return Oid{}, fmt.Errorf("oid is null")
 	}
-	ptr := C.pmemobj_direct(oid)
-	return *(*int)(ptr), nil
+	b.Set(C.pmemobj_direct(oid))
+	return Oid{oid}, nil
+}
+
+// NextBlob returns next element of provided Oid
+func (o *Oid) NextBlob(b Blob) (Oid, error) {
+	oid := C.pmemobj_next(o.oid)
+	if oid.off == 0 {
+		return Oid{}, fmt.Errorf("oid is null")
+	}
+	b.Set(C.pmemobj_direct(oid))
+	return Oid{oid}, nil
 }
--- pmemobj/pmemobj_test.go
+++ pmemobj/pmemobj_test.go
@@ -1,139 +1,262 @@
-/*
- * Copyright 2018, Intel Corporation
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in
- *       the documentation and/or other materials provided with the
- *       distribution.
- *
- *     * Neither the name of the copyright holder nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package pmemobj
 
 import (
 	"os"
 	"testing"
+	"unsafe"
 )
 
 const poolPath = "pool"
 
-func tearDown(p Pool) {
-	p.Close()
+type blobInt struct {
+	data int
+}
+
+func (b blobInt) Ptr() unsafe.Pointer {
+	return unsafe.Pointer(&b.data)
+}
+func (b blobInt) Size() uintptr {
+	return unsafe.Sizeof(b.data)
+}
+func (b *blobInt) Set(p unsafe.Pointer) {
+	b.data = *(*int)(p)
+}
+
+type myStruct struct {
+	intData    int
+	stringData string
+}
+
+type blobStruct struct {
+	my myStruct
+}
+
+func (b blobStruct) Ptr() unsafe.Pointer {
+	return unsafe.Pointer(&b.my)
+}
+func (b blobStruct) Size() uintptr {
+	return unsafe.Sizeof(b.my)
+}
+func (b *blobStruct) Set(p unsafe.Pointer) {
+	b.my = *(*myStruct)(p)
+}
+
+func helperCreateT(t *testing.T) Pool {
+	pool, err := Create(poolPath, "", 10*1024*1024, 0666)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return pool
+}
+
+func helperCreateB(b *testing.B) Pool {
+	pool, err := Create(poolPath, "", 10*1024*1024, 0666)
+	if err != nil {
+		b.Fatal(err)
+	}
+	return pool
+}
+
+func tearDown(p *Pool) {
+	if p.open {
+		p.Close()
+	}
 	os.Remove(poolPath)
 }
 
 func TestCreate(t *testing.T) {
-	pool, err := Create(poolPath, "", 10*1024*1024, 0666)
-	if err != nil {
-		t.Error(err)
-	}
-	defer tearDown(pool)
+	pool := helperCreateT(t)
+	defer tearDown(&pool)
 }
 
 func TestOpen(t *testing.T) {
-	pool, err := Create(poolPath, "", 10*1024*1024, 0666)
-	if err != nil {
-		t.Error(err)
-	}
-	defer tearDown(pool)
+	pool := helperCreateT(t)
+	defer tearDown(&pool)
 
 	pool.Close()
-	pool, err = Open(poolPath, "")
+	pool, err := Open(poolPath, "")
 	if err != nil {
 		t.Error(err)
 	}
 }
 
 func TestReopen(t *testing.T) {
-	pool, err := Create(poolPath, "", 10*1024*1024, 0666)
-	if err != nil {
-		t.Error(err)
-	}
-	defer tearDown(pool)
+	var pool Pool
+	var err error
+
+	pool = helperCreateT(t)
+	defer tearDown(&pool)
 
 	for i := 0; i < 3; i++ {
 		pool.Close()
 		pool, err = Open(poolPath, "")
 		if err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 	}
 }
 
-func TestAlloc(t *testing.T) {
-	pool, err := Create(poolPath, "", 10*1024*1024, 0666)
+func TestOpenUnpermitted(t *testing.T) {
+	pool, err := Create(poolPath, "", 10*1024*1024, 0000)
 	if err != nil {
+		t.Fatal(err)
+	}
+	defer tearDown(&pool)
+
+	pool.Close()
+
+	pool, err = Open(poolPath, "")
+
+	if pool.pop != nil {
+		t.Error("pop should be equal to nil")
+	}
+	if err == nil {
+		t.Error("No permission denied error")
+	}
+}
+
+func TestCreateTooSmall(t *testing.T) {
+	pool, err := Create(poolPath, "", 1*1024, 0666)
+	if pool.pop != nil {
+		t.Error("pop should be equal to nil")
+	}
+	if err == nil {
 		t.Error(err)
 	}
-	defer tearDown(pool)
+	defer tearDown(&pool)
+}
 
-	var oid Oid
-	oid, err = pool.Alloc(888)
+func TestAllocInt(t *testing.T) {
+	pool := helperCreateT(t)
+	defer tearDown(&pool)
+
+	b := blobInt{3}
+
+	oid, err := pool.AllocBlob(&b)
 
 	if err != nil {
 		t.Errorf("%v. Oid: %v", err, oid)
 	}
 }
+func TestReadInt(t *testing.T) {
+	pool := helperCreateT(t)
+	defer tearDown(&pool)
 
-func TestRead(t *testing.T) {
-	var err error
-	var pool Pool
-	pool, err = Create(poolPath, "", 10*1024*1024, 0666)
-	if err != nil {
-		t.Error(err)
-	}
-	defer tearDown(pool)
-
-	var oid Oid
-	allocated := 888
-	oid, err = pool.Alloc(allocated)
-
+	bIn := blobInt{888}
+	oid, err := pool.AllocBlob(&bIn)
 	if err != nil {
 		t.Errorf("%v. Oid: %v", err, oid)
 	}
 
-	var val int
-	val, err = pool.First()
+	var bOut blobInt
+	_, err = pool.FirstBlob(&bOut)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
-	if val != allocated {
-		t.Errorf("Val is equal %d but should be %d", val, allocated)
+
+	if bOut.data != bIn.data {
+		t.Errorf("Val is equal %d but should be %d", bOut.data, bIn.data)
+	}
+}
+
+func TestReadStruct(t *testing.T) {
+	pool := helperCreateT(t)
+	defer tearDown(&pool)
+
+	bIn := blobStruct{myStruct{888, "mystring"}}
+	oid, err := pool.AllocBlob(&bIn)
+	if err != nil {
+		t.Errorf("%v. Oid: %v", err, oid)
+	}
+
+	var bOut blobStruct
+	_, err = pool.FirstBlob(&bOut)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if bOut.my != bIn.my {
+		t.Errorf("Val is equal %v but should be %v", bOut.my, bIn.my)
 	}
 }
 
 func TestReadEmpty(t *testing.T) {
-	pool, err := Create(poolPath, "", 10*1024*1024, 0666)
-	if err != nil {
-		t.Error(err)
-	}
-	defer tearDown(pool)
+	pool := helperCreateT(t)
+	defer tearDown(&pool)
 
-	_, err = pool.First()
+	_, err := pool.FirstBlob(nil)
+
 	if err == nil {
 		t.Error("No error despite reading from empty pool")
+	}
+}
+
+func TestReadByNext(t *testing.T) {
+	pool := helperCreateT(t)
+	defer tearDown(&pool)
+	var b blobInt
+	var err error
+
+	vals := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, v := range vals {
+		b.data = v
+		_, err = pool.AllocBlob(&b)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	var oid Oid
+	for i, v := range vals {
+		if i == 0 {
+			oid, err = pool.FirstBlob(&b)
+		} else {
+			oid, err = oid.NextBlob(&b)
+		}
+		if err != nil {
+			t.Fatal(err)
+		}
+		if b.data != v {
+			t.Errorf("Val is equal %v but should be %v", b.data, v)
+		}
+	}
+}
+
+func TestReadEmptyByNext(t *testing.T) {
+	pool := helperCreateT(t)
+	defer tearDown(&pool)
+	var b blobInt
+	var err error
+
+	numAllocs := 3
+	for i := 0; i <= numAllocs; i++ {
+		b.data = i
+		_, err = pool.AllocBlob(&b)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	var oid Oid
+	for i := 0; i <= numAllocs+1; i++ {
+		if i == 0 {
+			oid, err = pool.FirstBlob(&b)
+		} else {
+			oid, err = oid.NextBlob(&b)
+		}
+		if i == numAllocs+1 {
+			if err == nil {
+				t.Error("Reading non-existent value should have returned error")
+			}
+		} else {
+			if err != nil {
+				t.Fatal(err)
+			}
+			if b.data != i {
+				t.Errorf("Val is equal %v but should be %v", b.data, i)
+			}
+		}
 	}
 }
 
@@ -143,34 +266,45 @@ func BenchmarkCreate(b *testing.B) {
 
 		b.StopTimer()
 		if err != nil {
-			b.Error(err)
+			b.Fatal(err)
 		}
-		tearDown(pool)
+		tearDown(&pool)
 		b.StartTimer()
 	}
 }
 
 func BenchmarkOpen(b *testing.B) {
-	pool, err := Create(poolPath, "", 10*1024*1024, 0666)
-	if err != nil {
-		b.Error(err)
-	}
+	pool := helperCreateB(b)
+	defer tearDown(&pool)
 	pool.Close()
+
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		pool, err = Open(poolPath, "")
+		pool, err := Open(poolPath, "")
 
 		b.StopTimer()
 
 		if err != nil {
-			b.Error(err)
-			os.Remove(poolPath)
+			b.Fatal(err)
 		}
 		pool.Close()
 
 		b.StartTimer()
 	}
+}
 
-	os.Remove(poolPath)
+func BenchmarkAlloc(b *testing.B) {
+	pool := helperCreateB(b)
+	defer tearDown(&pool)
+	blob := blobInt{999}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, err := pool.AllocBlob(&blob)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
 }
